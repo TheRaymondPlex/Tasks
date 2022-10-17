2cadb771917afcdca44cefba6fca07884a8e3c9a
TheRaymondPlex <TheRaymondPlex@gmail.com>
2022-10-17 14:34:15 +0200

    Upload Task3

---
+++ training-mvc.com/application/models/User.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace application\models;
+
+use application\core\Model;
+
+class User extends Model
+{
+    public function createNewUser($email, $name, $gender, $status) {
+        $result = $this->db->query("
+            INSERT INTO users(email, name, gender, status, addDate)
+            VALUES ('$email', '$name', '$gender', '$status', NOW())
+        ");
+        return $result;
+    }
+
+    public function updateUserByEmail($email, $name, $gender, $status, $emailOld) {
+        $result = $this->db->query("
+            UPDATE users
+            SET email='$email', name='$name', gender='$gender', status='$status'
+            WHERE email='$emailOld';
+        ");
+        return $result;
+    }
+
+    public function getUserByEmail($email) {
+        $result = $this->db->row("
+            SELECT * FROM users
+            WHERE email='$email';
+        ");
+        return $result;
+    }
+
+    public function deleteUserByEmail($email) {
+        $result = $this->db->query("
+            DELETE FROM users
+            WHERE email='$email';
+        ");
+        return $result;
+    }
+}
---
+++ training-mvc.com/index.php
@@ -0,0 +1,21 @@
+<?php
+
+use application\core\Router;
+use application\lib\DotEnv;
+
+spl_autoload_register(function($class) {
+    $path = str_replace('\\', '/', $class.'.php');
+
+    if (file_exists($path)) {
+        require $path;
+    }
+});
+
+session_start();
+
+(new DotEnv(__DIR__ . '/.env'))->load();
+
+$router = new Router;
+$router->run();
+
+
