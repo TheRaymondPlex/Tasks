1ec67e99daa9c8c83354382355be50e61f1c0bc7
TheRaymondPlex <TheRaymondPlex@gmail.com>
2022-10-17 14:08:33 +0200

    Removed Previous Commits

---
+++ training-mvc.com/application/config/db.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'host' => getenv('DB_HOST'),
+    'name' => getenv('DB_NAME'),
+    'user' => getenv('DB_USER'),
+    'password' => getenv('DB_PASSWORD'),
+];
---
+++ training-mvc.com/application/config/routes.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    '' => [
+        'controller' => 'main',
+        'action' => 'index',
+    ],
+
+    'user/create' => [
+      'controller' => 'user',
+      'action' => 'create',
+    ],
+
+    'user/edit' => [
+        'controller' => 'user',
+        'action' => 'edit',
+    ],
+
+    'user/delete' => [
+        'controller' => 'user',
+        'action' => 'delete',
+    ]
+];
